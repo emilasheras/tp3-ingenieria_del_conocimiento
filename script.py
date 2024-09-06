6be231890df53cdf4805705ffd2468cbd986131c
elasheras <emperorbfd@gmail.com>
2024-09-06 18:28:56 -0300

    first comm

---
+++ script.py
@@ -0,0 +1,34 @@
+# 1. Crea un código que imprima en pantalla la expresión:
+#    "Mi Primer Código En Python."
+print("Mi Primer Código En Python")
+
+# 2. Crea un código que le permita ingresar una respuesta al usuario haciéndole la siguiente pregunta:
+#    "¿Qué estás estudiando?"
+#    El código debe poder imprimir en pantalla lo ingresado por el usuario.
+response = input("¿Qué estás estudiando? (responde a continuación): ")
+print(response)
+
+# 3. Crea un código que le permita ingresar una respuesta al usuario haciéndole la siguiente pregunta:
+#    "¿En qué país vives?"
+#    El código debe poder imprimir en pantalla lo ingresado por el usuario.
+
+
+# 4. Declara dos variables llamadas `nombre` y `edad`.
+#    Asigna a `nombre` el valor "David Bowman" y a `edad` el valor 51.
+
+# 5. Crea tres variables: `nombre`, `apellido`, `nombrecompleto`.
+#    A `nombre` asígnale el valor "Julia" y en `apellido` el valor "Roberts".
+#    Finalmente construye `nombrecompleto` concatenando las variables (recuerda sumar un espacio intermedio).
+
+# 6. Declara la variable `materia` y asígnale el valor "Ingeniería del conocimiento".
+#    Muestra en pantalla la frase: "Estás estudiando 'materia'" (concatenando).
+
+# 7. Convierte el valor de `num1` (num1=35) en un int e imprime el tipo de dato que resulta.
+
+# 8. Imprime el nombre y número de asociado dentro de la siguiente frase:
+#    "Estimado/a (nombre_asociado) su número de asociado es: (numero_asociado)".
+
+# 9. Muestra en pantalla el cociente (división al piso) de los siguientes dos números:
+#    874 dividido entre 27. Debes mostrar solo el valor numérico que resulta de esta operación.
+
+# 10. Redondea el número 10.676767 al entero más próximo y muestra en pantalla el resultado.
